3925f14ebd4258ea5e21a80e29a3b4011ae0853e
PedroSodreUFES <pedro.malini@edu.ufes.br>
2024-12-22 18:46:51 -0300

    22/12 done

--- App.tsx
+++ App.tsx
@@ -1,16 +1,23 @@
-import { Text, View, StatusBar } from 'react-native';
+import { View, StatusBar } from 'react-native';
 import { useFonts, Roboto_700Bold, Roboto_400Regular } from '@expo-google-fonts/roboto';
+import { GluestackUIProvider, Text } from '@gluestack-ui/themed'; //Text da biblioteca
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_700Bold, Roboto_400Regular })
 
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center", backgroundColor:"#202024" }}>
-      
-      
-      <StatusBar barStyle="light-content" translucent backgroundColor="transparent"/>
-      
-      {fontsLoaded ? <Text>App da academia!</Text> : <View />}
-    </View>
+    <GluestackUIProvider>
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center", backgroundColor: "#202024" }}>
+
+
+        <StatusBar
+          barStyle="light-content"
+          translucent
+          backgroundColor="transparent"
+        />
+
+        {fontsLoaded ? <Text>App da academia!</Text> : <View />}
+      </View>
+    </GluestackUIProvider>
   );
 }
